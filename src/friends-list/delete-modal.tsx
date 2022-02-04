c84e02de74b3d18981490e6edca32bf2285fafec
tusharjo <tushar.jo@endurance.com>
2022-02-04 21:20:27 +0530

    Removed console

--- src/friends-list/delete-modal.tsx
+++ src/friends-list/delete-modal.tsx
@@ -3,7 +3,6 @@ import "@reach/dialog/styles.css";
 import styles from '../common/ListStyles.module.css';
 
 export const DeleteModal = ({ friendName, show, hide, onClick }: any) => {
-  console.log(friendName, "friendStat0]")
   return show ? <Dialog isOpen={show} onDismiss={hide}>
     <p>Remove <span style={{ fontWeight: 600 }}>{friendName}</span> from your friend list?</p>
     <button
